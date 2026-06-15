6187eb6d90d6dc4ae8a6c6bfc366a6bbd34f5834
Satakun Utama <linesofcodes@dailitation.xyz>
2026-06-15 14:20:47 +0700

    [web] Custom software support, Support uploading mods, [wip] The ability to import servers

--- QSM.Core/ServerSettings/ModpackInfo.cs
+++ QSM.Core/ServerSettings/ModpackInfo.cs
@@ -2,8 +2,18 @@ namespace QSM.Core.ServerSettings;
 
 public class ModpackInfo
 {
-	public string Id { get; set; }
-	public string Name { get; set; }
-	public string VersionId { get; set; }
-	public string VersionName { get; set; }
+	public ModpackInfo() {}
+	
+	public ModpackInfo(string id, string name, string versionId, string versionName)
+	{
+		Id = id;
+		Name = name;
+		VersionId = versionId;
+		VersionName = versionName;
+	}
+	
+	public string Id { get; set; } = string.Empty;
+	public string Name { get; set; } = string.Empty;
+	public string VersionId { get; set; } = string.Empty;
+	public string VersionName { get; set; } = string.Empty;
 }
